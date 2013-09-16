c75c94366155d524ce4ca3fd19eb96c6e8bd6f6e
Conis <conis.yi@gmail.com>
2013-09-16 11:40:07 +0800

    更新npm及测试用例

--- test/index.js
+++ test/index.js
@@ -1,9 +1,18 @@
 var expect = require('expect.js');
 var maker = require('../lib/index');
+var path = require('path');
 
-describe('测试用例', function(){
+describe('测试使用Sql语句', function(){
 	it('', function(){
+		return;
 		var sql = ['CREATE TABLE if not exists todolist(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, timestamp FLOAT, done boolean DEFAULT false, todo TEXT, todo_level integer)'];
-		maker.make(sql,  "/Users/conis/WorkStation/project/Peak/PeakSqlite/");
+		maker.makeWithSql(sql,  "/Users/conis/WorkStation/project/Peak/PeakSqlite/");
+	});
+});
+
+describe('测试根据sqlite数据库生成生成', function(){
+	it('', function(){
+
+		//maker.makeWithConfig(path.join(__dirname, 'PeakSqlite.json'));
 	});
 });
